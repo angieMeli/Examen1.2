23292c1ddc9ccfd210a16b63cf4f1916cc8282fd
angieMeli <angiemelissa131095@gmail.com>
2017-02-01 17:33:56 -0600

    base de Datos
    Angie Murillo Zuñiga

---
+++ progra3_exa2.sql
@@ -0,0 +1,151 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 01-02-2017 a las 23:26:02
+-- Versión del servidor: 5.7.14
+-- Versión de PHP: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `progra3_exa2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `author`
+--
+
+CREATE TABLE `author` (
+  `idAuthor` int(11) NOT NULL,
+  `name` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `author`
+--
+
+INSERT INTO `author` (`idAuthor`, `name`) VALUES
+(123, 'Marco'),
+(456, 'Pedro');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `authorcontact`
+--
+
+CREATE TABLE `authorcontact` (
+  `idAuthorContact` int(11) NOT NULL,
+  `idAuthor` int(11) NOT NULL,
+  `contact` text NOT NULL,
+  `type` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `book`
+--
+
+CREATE TABLE `book` (
+  `idBook` int(11) NOT NULL,
+  `idAuthor` int(11) NOT NULL,
+  `idType` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `calendar` date NOT NULL,
+  `price` float NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `book`
+--
+
+INSERT INTO `book` (`idBook`, `idAuthor`, `idType`, `name`, `calendar`, `price`) VALUES
+(323, 987, 123, 'marienela', '2017-02-10', 2500),
+(988, 0, 233, 'el gato negro', '2017-02-24', 3000);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `booktype`
+--
+
+CREATE TABLE `booktype` (
+  `idType` int(11) NOT NULL,
+  `type` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `booktype`
+--
+
+INSERT INTO `booktype` (`idType`, `type`) VALUES
+(777, 'historia'),
+(333, 'cultura');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `author`
+--
+ALTER TABLE `author`
+  ADD PRIMARY KEY (`idAuthor`);
+
+--
+-- Indices de la tabla `authorcontact`
+--
+ALTER TABLE `authorcontact`
+  ADD PRIMARY KEY (`idAuthorContact`);
+
+--
+-- Indices de la tabla `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`idBook`);
+
+--
+-- Indices de la tabla `booktype`
+--
+ALTER TABLE `booktype`
+  ADD PRIMARY KEY (`idType`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `author`
+--
+ALTER TABLE `author`
+  MODIFY `idAuthor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=457;
+--
+-- AUTO_INCREMENT de la tabla `authorcontact`
+--
+ALTER TABLE `authorcontact`
+  MODIFY `idAuthorContact` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `book`
+--
+ALTER TABLE `book`
+  MODIFY `idBook` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=989;
+--
+-- AUTO_INCREMENT de la tabla `booktype`
+--
+ALTER TABLE `booktype`
+  MODIFY `idType` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=778;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
